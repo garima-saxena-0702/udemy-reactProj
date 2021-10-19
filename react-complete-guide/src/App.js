cd8f2b9036fbd0a4ed920a6a2ed650b1c5ebc710
Garima Saxena <garimasaxena0702@gmail.com>
2021-10-20 00:52:24 +0530

    Assignment 1

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expense = [
@@ -24,27 +24,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
+      <Expenses expense={expense}/>
     </div>
   );
 }
